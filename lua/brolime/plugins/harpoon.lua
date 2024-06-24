3e23de18deb4d2bc210b2a6113596a3d4c9a4144
Daniel Goulding <dgouldingpa@gmail.com>
2024-06-24 09:30:27 -0400

    add plugins and lsp, cmp and snip suport

--- lua/brolime/plugins/harpoon.lua
+++ lua/brolime/plugins/harpoon.lua
@@ -3,11 +3,11 @@ return {
     name = 'harpoon',
     lazy = true,
     config = function()
-        vim.keymap.set("n", "<leader>ha", mark.add_file)
-
+        local mark = require("harpoon.mark")
+        local ui = require("harpoon.ui")
         -- harpoon menu --
+        vim.keymap.set("n", "<leader>ha", mark.add_file)
         vim.keymap.set("n", "<leader>hl", ui.toggle_quick_menu)
-
         -- harpoon quick navigation --
         vim.keymap.set("n", "<leader>h1", function() ui.nav_file(1) end)
         vim.keymap.set("n", "<leader>h2", function() ui.nav_file(2) end)
