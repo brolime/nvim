0a08e8223b6e3bb5e9b47c2fa88120df42403914
Daniel Goulding <dgouldingpa@gmail.com>
2024-07-17 20:23:35 -0400

    add Rust support and fix cmp mappings

--- lua/brolime/plugins/lsp-config.lua
+++ lua/brolime/plugins/lsp-config.lua
@@ -1,24 +1,22 @@
-return{
+return {
     {
-        'williamboman/mason.nvim',
+        "williamboman/mason.nvim",
         lazy = false,
         config = function()
-            require('mason').setup()
+            require("mason").setup()
         end,
     },
     {
         "VonHeikemen/lsp-zero.nvim",
         branch = "v3.x",
         lazy = false,
-        config = function ()
+        config = function()
             local lsp = require("lsp-zero")
-            lsp.on_attach(
-                function (client, bufnr)
-                    lsp.default_keymaps({ buffer = bufnr })
-                end
-            )
+            lsp.on_attach(function(client, bufnr)
+                lsp.default_keymaps({ buffer = bufnr })
+            end)
         end,
-        init = function ()
+        init = function()
             vim.g.lsp_zero_extend_cmp = 0
             vim.g.lsp_zero_extend_lspconfig = 0
         end,
@@ -27,9 +25,9 @@ return{
         "hrsh7th/nvim-cmp",
         event = "InsertEnter",
         dependencies = {
-            { "L3MON4D3/LuaSnip" }
+            { "L3MON4D3/LuaSnip" },
         },
-        config = function ()
+        config = function()
             local lsp_zero = require("lsp-zero")
             lsp_zero.extend_cmp()
 
@@ -39,16 +37,18 @@ return{
             cmp.setup({
                 formatting = lsp_zero.cmp_format(),
                 mapping = cmp.mapping.preset.insert({
-                    ["<CR>"] = cmp.mapping.confirm({ select = false }),
+                    ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+                    ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+                    ["<C-y>"] = cmp.mapping.confirm({ select = true }),
                     ["<C-u>"] = cmp.mapping.scroll_docs(-4),
-                    ["<C-d>"] = cmp.mapping.scroll_docs(4)
+                    ["<C-d>"] = cmp.mapping.scroll_docs(4),
                 }),
-                preselect = 'item',
-                completion = { completeopt = 'menu,menuone,noinsert' },
+                preselect = "item",
+                completion = { completeopt = "menu,menuone,noinsert" },
                 sources = {
-                    { name = 'buffer' },
-                    { name = 'nvim_lsp' },
-                    { name = 'luasnip' },
+                    { name = "buffer" },
+                    { name = "nvim_lsp" },
+                    { name = "luasnip" },
                 },
             })
         end,
@@ -57,8 +57,8 @@ return{
         "neovim/nvim-lspconfig",
         lazy = false,
         dependencies = {
-            {"hrsh7th/cmp-nvim-lsp"},
-            {"williamboman/mason-lspconfig.nvim"},
+            { "hrsh7th/cmp-nvim-lsp" },
+            { "williamboman/mason-lspconfig.nvim" },
         },
         config = function()
             local lsp_zero = require("lsp-zero")
@@ -68,12 +68,36 @@ return{
             end)
 
             require("mason-lspconfig").setup({
-                ensure_installed = {"clangd", "lua_ls"},
+                ensure_installed = { "clangd", "lua_ls" },
                 handlers = {
                     lsp_zero.default_setup,
                     clangd = function()
                         require("lspconfig").clangd.setup({
-                            cmd = { 'clangd' },
+                            cmd = { "clangd" },
+                        })
+                    end,
+
+                    rust_analyzer = function()
+                        require("lspconfig").rust_analyzer.setup({
+                            on_attach = on_attach,
+                            settings = {
+                                ["rust-analyzer"] = {
+                                    imports = {
+                                        granularity = {
+                                            group = "module",
+                                        },
+                                        prefix = "self",
+                                    },
+                                    cargo = {
+                                        buildScripts = {
+                                            enable = true,
+                                        },
+                                    },
+                                    procMacro = {
+                                        enable = true,
+                                    },
+                                },
+                            },
                         })
                     end,
 
@@ -81,30 +105,32 @@ return{
                         require("lspconfig").lua_ls.setup({
                             on_init = function(client)
                                 local path = client.workspace_folders[1].name
-                                if vim.loop.fs_stat(path .. '/.luarc.json') or vim.loop.fs_stat(path .. '/.luarc.jsonrc') then
+                                if
+                                    vim.loop.fs_stat(path .. "/.luarc.json")
+                                    or vim.loop.fs_stat(path .. "/.luarc.jsonrc")
+                                then
                                     return
                                 end
 
                                 client.config.settings.Lua = vim.tbl_deep_exten("force", client.config.settings.Lua, {
                                     runtime = {
-                                        version = 'LuaJIT'
+                                        version = "LuaJIT",
                                     },
                                     workspace = {
                                         checkThirdParty = false,
                                         library = {
-                                            vim.env.VIMRUNTIME
-                                        }
-                                    }
+                                            vim.env.VIMRUNTIME,
+                                        },
+                                    },
                                 })
                             end,
                             settings = {
-                                Lua = {}
-                            }
+                                Lua = {},
+                            },
                         })
                     end,
-                }
+                },
             })
         end,
-    }
+    },
 }
-
